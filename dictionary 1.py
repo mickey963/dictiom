03740b22fcab7a541a7cff3c718bfafe2c52da6e
mickey963 <michaelopute9@gmail.com>
2025-01-19 20:15:35 +0100

    Add files via upload

---
+++ dictionary 1.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+# Dictionary with translations
+translations = {
+    "Spanish": {"hello": "hola", "goodbye": "adiós", "please": "por favor", "thank you": "gracias", "yes": "si", "no": "no","sorry": "lo siento",
+                "good morning": "Buenos días", "good night": "Buenas noches", "how are you": "¿Cómo estás", "i'm fine": "Estoy bien","water": "agua",
+               "food": "comida", "help": "Ayuda", "friend": "amigo","excuse me": "pardon", "my name is": "Me llamo...", "How much is it?":"¿Cuánto cuesta?",
+                "What’s your name": "¿Cómo te llamas?", "welcome": "Bienvenido/a", "home": "casa", "happy": "feliz"},
+
+    "French": {"hello": "bonjour", "goodbye": "au revoir", "please": "S'il vous plaît", "thank you": "merci", "yes": "oui", "no": "non","sorry": "Désolé(e)",
+                "good morning": "bonjour", "good night": "	Bonne nuit", "how are you": "Comment ça va ?", "i'm fine": "Ça va bien","water": "eau",
+               "food": "Nourriture", "help": "aide", "friend": "ami(e)","excuse me": "Excusez-moi", "my name is": "Je m'appelle...", "How much is it?":"Combien ça coûte ?",
+                "What’s your name": "Comment vous appelez-vous ?", "welcome": "bienvenue", "home": "maison", "happy": "heureux"},
+
+
+# Function to translate"
+}
+def translate_word():
+    selected_language = language_selector.get()
+    english_word = english_entry.get().lower()
+
+    if selected_language == "choose a language":
+        messagebox.showerror("Error", " choose a language.")
+        return
+
+    if english_word in translations[selected_language]:
+        translated_word = translations[selected_language][english_word]
+        result_label.config(text=f'Translation: {translated_word}')
+    else:
+        result_label.config(text=" Translation not found.")
+
+# GUI setup
+root = tk.Tk()
+root.title("5 language dictionary")
+
+# Language selection dropdown
+language_label = tk.Label(root, text=" choose a language:")
+language_label.pack(pady=5)
+
+language_selector = ttk.Combobox(root, values=["Select Language"] + list(translations.keys()), state="readonly")
+language_selector.set("pick a language")
+language_selector.pack(pady=5)
+
+# English word input
+english_label = tk.Label(root, text="Enter English Word:")
+english_label.pack(pady=5)
+
+english_entry = tk.Entry(root)
+english_entry.pack(pady=5)
+
+# Translate button
+translate_button = tk.Button(root, text="Translate", command=translate_word)
+translate_button.pack(pady=10)
+
+# Result display
+result_label = tk.Label(root, text="", font=("Helvetica", 12))
+result_label.pack(pady=10)
+
+# Run the GUI
+root.mainloop()
